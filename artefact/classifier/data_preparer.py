b6a740518ebb2d255691536bac5d872d2a8815d3
Andrei Butnaru <abutnaru@outlook.com>
2020-04-12 16:31:43 +0100

    Fixed argument parsing

--- artefact/classifier/data_preparer.py
+++ artefact/classifier/data_preparer.py
@@ -15,14 +15,13 @@ def get_input():
     parser.add_argument("-r","--records", dest="records", help="Number of records", type=str, required=True)
     parser.add_argument("-o","--output", dest="filename", help="Output destination", type=str, required=True)
     args = parser.parse_args()
-    return (args.dataset, args.records, args.filename)
+    return (args.dataset, int(args.records), args.filename)
 
 
 def run():
     target_dataset, target_records, outfile = get_input()
-    reader = csv.reader(target_dataset)
-    features_file = open(outfile + ".csv", "w")
-    writer = csv.writer(features_file)
+    reader = csv.reader(open("data/"+target_dataset+ ".csv", 'r'))
+    writer = csv.writer(open(outfile + ".csv", "w"))
     malicious = 0
     benign = 0
     for row in reader:
@@ -30,11 +29,11 @@ def run():
         # Writing rows and keeping track of total
         # records written based on the label
         if label == "0" and benign <= (target_records / 2):
-            f = features.extract(url, label)
+            f = features.extract(url, 0)
             writer.writerow(f)
             benign += 1
         if label == "1" and malicious <= (target_records / 2):
-            f = features.extract(url, label)
+            f = features.extract(url, 1)
             writer.writerow(f)
             malicious += 1
         if (malicious + benign) % 1000 == 0:
