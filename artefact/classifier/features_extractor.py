6931db75732f30b53c0eaff2f2d9fec5510e96b5
Andrei Butnaru <abutnaru@outlook.com>
2020-04-17 23:51:04 +0100

    Reworked features

--- artefact/classifier/features_extractor.py
+++ artefact/classifier/features_extractor.py
@@ -8,6 +8,7 @@ import csv
 import numpy as np
 import subprocess
 import editdistance as levenshtein
+import urllib.parse as up
 
 
 def hamming_distance(chaine1, chaine2):
@@ -17,7 +18,7 @@ def hamming_distance(chaine1, chaine2):
     return len(list(filter(lambda x: ord(x[0]) ^ ord(x[1]), zip(chaine1, chaine2))))
 
 
-def min_hamming(url, N):
+def max_hamming(url, N):
     """
     Calculates the minimum value for the hamming distance
     between the top N domains and the URL's domain
@@ -46,63 +47,60 @@ def min_levenshtein(url, N):
     Calculates the minimum value for the levenshtein distance
     between the top N domains and the URL's domain and subdomain
     """
-    components = tld.extract(url)
-    subdomain = components.subdomain
-    domain = components.domain
-
-    f = open("data/benign_1M.csv")
-    reader = csv.reader(f)
-    min_distance = 9999
-    min_sub_distance = 9999
+    subdomain, domain = tld.extract(url)[:2]
+    with open("data/benign_1M.csv") as benign_list:
+        reader = csv.reader(benign_list)
+    min_dom_dist = 999
+    min_sub_dist = 999
     index = 0
-
     for row in reader:
         index += 1
         if index == N:
             break
-        record = tld.extract(row[0])
-        levdd = levenshtein.eval(domain, record.domain)
-        if levdd < min_distance:
-            min_distance = levdd
+        b_domain = tld.extract(row[0]).domain
+        levdd = levenshtein.eval(domain, b_domain)
+        if levdd < min_dom_dist:
+            min_dom_dist = levdd
         # Splitting the subdomain to cover cases
         # similar to https://example.amazon.domain.com/
         for component in subdomain.split("."):
-            levsd = levenshtein.eval(component, record.domain)
-            if levsd < min_sub_distance:
-                min_sub_distance = levsd
+            levsd = levenshtein.eval(component, b_domain)
+            if levsd < min_sub_dist:
+                min_sub_dist = levsd
     # Test based on the assumption that two strings with levenstain
     # distance greater than 10 can be regarded as different. Otherwise
     # the target URL's domain and subdomain are considered similar to
     # one of the top N domains
-    if min_distance > 10:
-        min_distance = 0
-    else:
-        min_distance = 1
-    if min_sub_distance > 10:
-        min_sub_distance = 0
-    else:
-        min_sub_distance = 1
+    min_dom_dist = 0 if min_dom_dist > 10 else 1
+    min_sub_dist = 0 if min_sub_dist > 10 else 1
 
-    return min_distance, min_sub_distance
+    return min_dom_dist, min_sub_dist
 
 
 def extract(url, label=-1):
     """
     Decomposes passed URL into the selected list of features
     """
+    parsed_url = up.urlparse(url)
+
     # Feature 1: URL size
     url_size = len(url)
 
-    # Feature 2: Number of '@', '-' and '~' characters
-    symbols_count = url.count("@") + url.count("-") + url.count("~")
-
     # Feature 3: Number of dots
-    dot_count = url.count(".")
+    netloc_dot_count = parsed_url.netloc.count(".")
+    path_dot_count = parsed_url.path.count(".") + parsed_url.query.count(".")
+
+    # Feature 2: Number of '@', '-' and '~' characters
+    at_count = url.count("@")
+    hyphen_count = (
+        parsed_url.netloc.count("-") if path_dot_count < 2 else url.count("-")
+    )  # Popular in phishing domains
+    tilde_count = url.count(
+        "~"
+    )  # Should not be used https://www.ietf.org/rfc/rfc2396.txt
 
     # Feature 4: Number of numerical characters
-    digit_count = sum(
-        c.isdigit() for c in tld.extract(url).subdomain + tld.extract(url).domain
-    )
+    digit_count = sum(c.isdigit() for c in parsed_url.netloc)
 
     # Feature 5: Sensitive vocabulary
     sensitive_vocabulary = [
@@ -121,9 +119,7 @@ def extract(url, label=-1):
             sens_word_count += 1
 
     # Feature 6: Presence of IP address in URL
-    ip_presence = 0
-    if len(re.findall(r"[0-9]+(?:\.[0-9]+){3}", url)) != 0:
-        ip_presence = 1
+    ip_presence = 1 if len(re.findall(r"[0-9]+(?:\.[0-9]+){3}", url)) != 0 else 0
 
     # Feature 7 and 8: Minimum distance between URL's domain and
     # subdomain and the top N benign domains
