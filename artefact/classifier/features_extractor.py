d39d18c1c8c879ab4f1b9fdb4a0c8c39183174f3
Andrei Butnaru <abutnaru@outlook.com>
2020-04-24 20:57:31 +0100

    Added feature description

--- artefact/classifier/features_extractor.py
+++ artefact/classifier/features_extractor.py
@@ -1,148 +1,174 @@
 """
 The features extractor decomposes each URL in the set of selected 
-features and saves the records in a CSV file. Selected features are: 
+features and saves the records in a CSV file. Selected features are: URL
+size, protocol used (HTTP/HTTPS), domain and subdomain digit count,
+symbols count ('@', '~'), presence
 """
 import csv
 import re
 import subprocess
-import urllib.parse as up
+import urllib.parse as urllib
 
 import editdistance as levenshtein
 import numpy as np
 import tldextract as tld
 
 
-def hamming_distance(chaine1, chaine2):
-    return len(list(filter(lambda x: ord(x[0]) ^ ord(x[1]), zip(chaine1, chaine2))))
+def hamming_distance(s1, s2):
+    return len(list(filter(lambda x: ord(x[0]) ^ ord(x[1]), zip(s1, s2))))
 
 
-def min_hamming(url, N):
+def min_distances(url, N):
     """
-    Calculates the minimum value for the levenshtein distance between 
-    the top N domains and the URL's domain and subdomain. To ensure 
-    precision, the subdomain is split on "." and "-" and then fed into 
-    the levenshtein distance calculation.
+    Calculates the minimum value for the levenshtein distance between
+    the top N domains and the URL's domain and the hamming distance
+    between the top N domains and URL's subdomain. To ensure precision,
+    the subdomain is split on "." and "-" and then fed into the hamming
+    distance calculation.
     """
     subdomain, domain = tld.extract(url)[:2]
-    # with open("data/benign_1M.csv") as benign_list:
-    #     reader = csv.reader(benign_list)
-    reader = csv.reader(open("data/processed_sets/benign_1M.csv"))
-    min_dom_dist = 999
-    min_sub_dist = 999
-    index = 0
-    for row in reader:
-        index += 1
-        if index == N:
-            break
+    f_benign_1M = open("data/processed_sets/benign_1M.csv")
+
+    min_dd, min_sd = 100, 100
+    index = 1
+    for row in csv.reader(f_benign_1M):
         b_domain = tld.extract(row[0]).domain
         levdd = levenshtein.eval(domain, b_domain)
-        if levdd < min_dom_dist:
-            min_dom_dist = levdd
-        # Splitting the subdomain to cover cases
-        # similar to https://example.amazon.domain.com/
+        min_dd = levdd if levdd < min_dd else min_dd
+
+        # Splitting the subdomain to cover cases similar to
+        # https://target-brand.customer-service.signin.example.com/
         for component in subdomain.split("."):
             if "-" in component:
                 for subcomp in component.split("-"):
-                    levsd = hamming_distance(subcomp, b_domain)
-                    if levsd < min_sub_dist:
-                        min_sub_dist = levsd
+                    hamsd = hamming_distance(subcomp, b_domain)
+                    min_sd = hamsd if hamsd < min_sd else min_sd
             else:
-                levsd = hamming_distance(component, b_domain)
-                if levsd < min_sub_dist:
-                    min_sub_dist = levsd
+                hamsd = hamming_distance(component, b_domain)
+                min_sd = hamsd if hamsd > min_sd else min_sd
+        index += 1
+        if index == N:
+            break
+
     # Test based on the assumption that two strings with levenstain
     # distance greater than 10 can be regarded as different. Otherwise
     # the target URL's domain and subdomain are considered similar to
     # one of the top N domains
-    dom_dist = 1 if min_dom_dist <= 5 and min_dom_dist != 0 else 0
-    sub_dist = (
-        1
-        if min_sub_dist <= 5
-        and "www" not in subdomain
-        and len(subdomain.split(".")) == 1
-        else 0
-    )
-
-    return dom_dist, sub_dist
+    sd = 1
+    dd = 1 if min_dd <= 5 and min_dd != 0 else 0
+    if (len(subdomain.split(".")) == 1 and subdomain == "www") or min_sd > 5:
+        sd = 0
+    return dd, sd
 
 
-def get_occurences(words, target):
-    for word in words:
-        if word in target:
-            return 1
-    return 0
+def has_domain(url, N):
+    f_benign_list = open("data/processed_sets/benign_1M.csv")
+    index = 1
+
+    for row in csv.reader(f_benign_list):
+        # Testing first if the length of the domain is greater or equal
+        # to 5 to reduce false positives produced by domains like t.com
+        if len(row[0]) >= 5 and (row[0] in url.path or row[0] in url.query):
+            return True
+        index += 1
+        if index == N:
+            break
+
+    f_benign_list.close()
+    return False
 
 
-def has_domain(target_path, N):
-    with open("data/processed_sets/benign_1M.csv") as benign_list:
-        index = 0
-        reader = csv.reader(benign_list)
-        for row in reader:
-            index += 1
-            if len(row[0]) >= 5 and row[0] in target_path:
+def contains(words, target, check_domains=False):
+    if check_domains:
+        for word in words:
+            if word in target:
                 return 1
-            if index == N:
-                break
+        return 1 if has_domain(target, 2500) or has_domain(target, 2500) else 0
+    else:
+        for word in words:
+            if word in target:
+                return 1
         return 0
 
 
-def extract(url, label=-1):
+def extract(raw_url, label=-1):
     """
     Decomposes passed URL into the selected list of features
     """
-    parsed_url = up.urlparse(url)
-
-    # Feature 1: URL size
-    url_size = len(url)
-
-    proto = 0 if url.split(":")[0] == "https" else 1
-
-    # Feature 3: Number of dots
-    # netloc_dot_count = parsed_url.netloc.count(".")
+    parsed_url = urllib.urlparse(raw_url)
+    netloc = tld.extract(raw_url)
     path_dot_count = parsed_url.path.count(".") + parsed_url.query.count(".")
 
-    # Feature 2: Number of '@', '-' and '~' characters
-    symbols_count = url.count("@") + url.count("~")
-    domain_in_path = 0 if path_dot_count < 2 else 1
-    hyphen_count = (
-        parsed_url.netloc.count("-") if path_dot_count < 2 else url.count("-")
-    )  # Popular in phishing domains
-    # tilde_count = url.count("~")  # Should not be used https://www.ietf.org/rfc/rfc2396.txt
+    ### Feature 1: URL size
+    # Reasoning: The literature shows a clear discrepancy between the
+    # average benign URL size and average phishing URL size.
+    f1 = len(raw_url)
 
-    # Feature 2: Number of '@', '-' and '~' characters
-    # redirect = 0
-    # redirect_cues = ["url", "redirect", "forward", "#btnI&q"]
-    # for cue in redirect_cues:
-    #    if cue in parsed_url.path:
-    #        redirect = 1
-    # Feature 4: Number of numerical characters
-    digit_count = sum(c.isdigit() for c in parsed_url.netloc)
+    ### Feature 2: Protocol used (HTTP/HTTPS)
+    # Reasoning: Although the number of phishing websites has risen
+    # significantly in recent years, serving a website through HTTP is
+    # still an indicator of maliciousness in most cases.
+    f2 = 0 if raw_url.split(":")[0] == "https" else 1
 
-    # Feature 4: Size of the subdomain
-    subdomain_components = (
-        tld.extract(url).subdomain.count(".")
-        + tld.extract(url).subdomain.count("-")
-        + 1
-    )
-    long_subdomain = 1 if subdomain_components >= 3 else 0
+    ### Feature 3: Number of numerical characters
+    # Reasoning: It is highly uncommon for benign domains and subdomains
+    # to contain any numerical characters.
+    f3 = sum(c.isdigit() for c in parsed_url.netloc)
 
-    # Feature 5: Sensitive vocabulary
+    ### Feature 4: Number of '@' and '~' characters
+    # Reasoning: Like numerical characters, it is uncommon for an URL to
+    # contain any '~' characters. The '@' character produces unexpected
+    # behaviour in the browser. It could either redirect to an email
+    # address or get the browser to ignore everything after it.
+    f4 = raw_url.count("@") + raw_url.count("~")
+
+    # Feature 5: Domain presence in the url path segment
+    # Reasoning: This feature determines whether the path contains a
+    # domain. This practice is often found in redirects and domains
+    # hosted on places like firebase or storage.googleapis.com.
+    f5 = 0 if path_dot_count < 2 else 1
+
+    ### Feature 6: Number of hyphens
+    # Reasoning: It is uncommon for benign domains and subdomains to use
+    # hyphens to separate words while it is common for phishing URLs to
+    # use them. If a domain is detected in path, the number of hyphens
+    # is counted over the entire URL
+    f6 = parsed_url.netloc.count("-") if f5 else raw_url.count("-")
+
+    # Feature 7: Size of the subdomain
+    # Reasoning: The literature shows that there is a strong correlation
+    # between a logn subdomain and phishing webpages. In this feature
+    # the length of the subdomain is based on its components rather than
+    # character count as past experiments proved it more effective
+    components = netloc.subdomain.count(".") + netloc.subdomain.count("-") + 1
+    f7 = 1 if components >= 3 else 0
+
+    # Features 8,9 and 10: Presence of sensitive vocabulary and of
+    # benign domains in URL's subdomain
+    # Reasoning: The sensitive words shown below are a curated selection
+    # from the output of a word frequency algorithm and are an indicator
+    # of suspiciousness. Because it is a common practice for phishing
+    # URLs to inlcude the target brand in their subdomain, the sensitive
+    # subdomain words check the presence of top N benign domains as well
     sensitive_subdomains = [
         "paypal",
         "sites",
         "secure",
         "login",
-        "runescape" "account",
-        "service" "sign",
+        "runescape",
+        "account",
+        "service",
+        "sign",
         "bank",
         "transfer",
         "user",
         "security",
     ]
     sensitive_domains = [
-        "000webhost",  #  you can set up own domain on 000webhost
-        "sharepoint",  # not recommended https://support.office.com/en-us/article/SharePoint-Online-Public-Websites-to-be-discontinued-e86bfd2f-5c7d-446f-a430-7cfcc0130916
-        "customer" "service",
+        "000webhost",
+        "sharepoint",
+        "customer",
+        "service",
         "secure",
         "support",
     ]
@@ -159,57 +185,22 @@ def extract(url, label=-1):
         "bank",
         "verify",
     ]
-    subdomain_occs = get_occurences(sensitive_subdomains, tld.extract(url).subdomain)
-    domain_occs = get_occurences(sensitive_domains, tld.extract(url).domain)
-    path_occs = get_occurences(sensitive_paths, parsed_url.path)
-    if path_occs == 0:
-        path_occs = (
-            1
-            if has_domain(parsed_url.path, 2500) or has_domain(parsed_url.query, 2500)
-            else 0
-        )
+    f8 = contains(sensitive_subdomains, netloc.subdomain)
+    f9 = contains(sensitive_domains, netloc.domain)
+    f10 = contains(sensitive_paths, parsed_url, check_domains=True)
 
-    # Feature 6: Presence of IP address in URL
-    ip_presence = 1 if len(re.findall(r"[0-9]+(?:\.[0-9]+){3}", url)) != 0 else 0
+    # Feature 11: Presence of IP address in URL
+    # Reasoning: Usage of IP address instead of a domain name is tighyly
+    # related to phishing/malicious intent.
+    f11 = 1 if len(re.findall(r"[0-9]+(?:\.[0-9]+){3}", raw_url)) != 0 else 0
 
-    # Feature 7 and 8: Minimum distance between URL's domain and
+    # Feature 12 and 13: Minimum distance between URL's domain and
     # subdomain and the top N benign domains
-    dom_distance, subdom_distance = min_hamming(url, 25_000)
+    # Reasoning: It is a common practice for phishing URLs to use a
+    # variation of the targeted domain either in their domain or
+    # subdomain to create the illusion of the legit website.
+    f12, f13 = min_distances(raw_url, 25_000)
 
-    if label < 0:
-        return np.array(
-            [
-                url_size,
-                proto,
-                digit_count,
-                symbols_count,
-                domain_in_path,
-                hyphen_count,
-                long_subdomain,
-                subdomain_occs,
-                domain_occs,
-                path_occs,
-                ip_presence,
-                dom_distance,
-                subdom_distance,
-            ]
-        )
-    else:
-        return np.array(
-            [
-                label,
-                url_size,
-                proto,
-                digit_count,
-                symbols_count,
-                domain_in_path,
-                hyphen_count,
-                long_subdomain,
-                subdomain_occs,
-                domain_occs,
-                path_occs,
-                ip_presence,
-                dom_distance,
-                subdom_distance,
-            ]
-        )
+    if label >= 0:
+        return np.array([label, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13])
+    return np.array([f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13])
